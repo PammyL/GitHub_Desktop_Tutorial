956f46d76a9ce486c8da5505faf14c441c7a0755
zli67666 <138759111+zli67666@users.noreply.github.com>
2024-07-08 13:25:36 +0800

    Revert "6666"
    This reverts commit dcd0fa0c065b4abf31a4cd547489b1bf83c0bada.

--- Ameba_Blink/Ameba_Blink.ino
+++ Ameba_Blink/Ameba_Blink.ino
@@ -11,5 +11,5 @@ void loop() {
   digitalWrite(LED_1, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
   digitalWrite(LED_1, LOW);   // turn the LED off by making the voltage LOW
-  delay(5000);   
+  delay(1000);   
 }
